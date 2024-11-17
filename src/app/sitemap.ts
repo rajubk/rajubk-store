8b39ae251bb943381dd8290b9743faaecaf40f8b
Raju BK <raju3be@gmail.com>
2024-11-17 11:31:05 +0000

    dfd

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,27 +7,6 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   return (products ?? []).map((x) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/${x.id}`,
   }));
-
-  return [
-    {
-      url: "https://acme.com",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    },
-    {
-      url: "https://acme.com/about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://acme.com/blog",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-  ];
 };
 
 export default sitemap;
