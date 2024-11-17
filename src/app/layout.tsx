b7b94ab73ea7c11fbe3f76809849b4b790fda904
Raju BK <raju3be@gmail.com>
2024-11-17 08:31:04 +0000

    fdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import ogImage from "@/app/opengraph-image.png";
 import "./globals.css";
 
 const geistSans = localFont({
