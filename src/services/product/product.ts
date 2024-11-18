ce62454c8197b1af0029be5d1ced66529ab6b573
Raju BK <raju3be@gmail.com>
2024-11-18 15:02:52 +0000

    dummi

--- src/services/product/product.ts
+++ src/services/product/product.ts
@@ -1,10 +1,12 @@
 import { GET_PRODUCTS } from "@/utils/endpoints";
 import { FETCH_PRODUCTS_TAG } from "./constats";
 import { Product } from "./type";
+import { logger } from "@/utils/pino";
 
 export const getProducts = async () => {
   try {
     const url = `${process.env.API_URL}/${GET_PRODUCTS}`;
+    logger.info("prodcut | fetch start");
     const res = await fetch(url, {
       next: { revalidate: 3600, tags: [FETCH_PRODUCTS_TAG] },
       headers: {
@@ -12,8 +14,11 @@ export const getProducts = async () => {
       },
     });
     const products: Product[] = await res.json();
+    logger.info("product| fetch sucess");
+
     return products;
   } catch (error) {
+    logger.error({ error }, "product | fetch error");
     throw new Error(`Failed to fetch error:${error}`);
   }
 };
