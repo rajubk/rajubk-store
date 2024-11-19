6ca64bdbde17037dcbd47ea766b507b0dee0dc11
rajubkuvce <raju.bk@hotmail.com>
2024-11-19 20:15:43 +0530

    dfd

--- src/app/components/notification/notification.tsx
+++ src/app/components/notification/notification.tsx
@@ -4,15 +4,20 @@ import { notificationContext } from "@/providers/notification-provider";
 import React, { useContext } from "react";
 
 const Notification = ({ error }: { error?: string }) => {
-  // const {
-  //   notification: { message, severity, variant },
-  // } = useContext(notificationContext);
+  const {
+    notification: { message, severity, variant },
+    clearNotification,
+  } = useContext(notificationContext);
 
-  // const notify = {
-  //   message: error || message,
-  //   severity: error ? "error" : severity,
-  //   variant: variant,
-  // };
+  clearNotification();
+
+  const notify = {
+    message: error || message,
+    severity: error ? "error" : severity,
+    variant: variant,
+  };
+
+  console.log(notify);
 
   return <div>Notification</div>;
 };
